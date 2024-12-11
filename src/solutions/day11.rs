17adaa37ca0ecf486b63aa764dc12bac4eed3c20
Bas Verhoeven <b.verhoeven@homewizard.nl>
2024-12-11 20:41:59 +0000

    Day 11: Replace my own memoization cache with "cached::proc_macro"
    By having a '#[cached]' macro, every call to the function passes
    through a cache. If the function is called, a cache is checked
    and if the exact arguments (stone, count) are already present
    then it'll return those.
    
    So... It basically does the same as my homegrown code did, but
    probably better.

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use cached::proc_macro::cached;
 
 fn parse_input(input: &Vec<String>) -> Vec<u64> {
     input
@@ -9,27 +9,23 @@ fn parse_input(input: &Vec<String>) -> Vec<u64> {
         .collect()
 }
 
-fn blink_recursive(stone: u64, rounds: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
-    cache.get(&(stone, rounds)).copied().unwrap_or_else(|| {
-        let stone_count = match rounds {
-            0 => 1,
-            _ => match stone {
-                0 => blink_recursive(1, rounds - 1, cache),
-                _ => match split_stone(stone) {
-                    Some((left_half, right_half)) => {
-                        let left = blink_recursive(left_half, rounds - 1, cache);
-                        let right = blink_recursive(right_half, rounds - 1, cache);
+#[cached]
+fn blink_recursive(stone: u64, rounds: u64) -> u64 {
+    match rounds {
+        0 => 1,
+        _ => match stone {
+            0 => blink_recursive(1, rounds - 1),
+            _ => match split_stone(stone) {
+                Some((left_half, right_half)) => {
+                    let left = blink_recursive(left_half, rounds - 1);
+                    let right = blink_recursive(right_half, rounds - 1);
 
-                        left + right
-                    }
-                    None => blink_recursive(stone * 2024, rounds - 1, cache),
-                },
+                    left + right
+                }
+                None => blink_recursive(stone * 2024, rounds - 1),
             },
-        };
-
-        cache.insert((stone, rounds), stone_count);
-        stone_count
-    })
+        },
+    }
 }
 
 fn split_stone(engraving: u64) -> Option<(u64, u64)> {
@@ -45,19 +41,15 @@ fn split_stone(engraving: u64) -> Option<(u64, u64)> {
 }
 
 pub fn part1(input: &Vec<String>) -> i64 {
-    let cache: &mut HashMap<(u64, u64), u64> = &mut HashMap::new();
-
     parse_input(input)
         .iter()
-        .map(|&stone| blink_recursive(stone, 25, cache))
+        .map(|&stone| blink_recursive(stone, 25))
         .sum::<u64>() as i64
 }
 
 pub fn part2(input: &Vec<String>) -> i64 {
-    let cache: &mut HashMap<(u64, u64), u64> = &mut HashMap::new();
-
     parse_input(input)
         .iter()
-        .map(|&stone_number| blink_recursive(stone_number, 75, cache))
+        .map(|&stone| blink_recursive(stone, 75))
         .sum::<u64>() as i64
 }
